9f6cdb693bd9465e589875acf3bcd9b4b2d5a3e3
Ryan S. Brown <sb@ryansb.com>
2014-10-21 19:28:03 -0400

    write counts periodically instead of on every request

--- typhoon/server/__init__.py
+++ typhoon/server/__init__.py
@@ -1,14 +1,20 @@
 import logging
+from collections import defaultdict
+
 import tornado.ioloop
-import tornado.web
+from tornado.options import options
 import tornado.httpserver
 import tornado.web
-from tornado.options import options
+
+from typhoon.server.clients.mongo_client import BaseMongoClient
 from typhoon.server.settings import settings
 from typhoon.server.url_patterns import url_patterns
 
 
 class App(tornado.web.Application):
+
+    _counters = defaultdict(int)
+    _client = None
 
     def __init__(self):
         """App wrapper constructor, global objects within our Tornado platform
@@ -16,6 +22,27 @@ class App(tornado.web.Application):
         self.logger = logging.getLogger(self.__class__.__name__)
 
         tornado.web.Application.__init__(self, url_patterns, **settings)
+
+        assert settings.get('db', None) is not None
+        self.client = BaseMongoClient('test', settings)
+
+        tornado.ioloop.PeriodicCallback(self.write_counter, 5000).start()
+
+    def write_counter(self):
+        self.client = BaseMongoClient('test', settings)
+        keys = list(self._counters.keys())
+        for k in keys:
+            self.logger.info("Writing %s" % k)
+            self.client.update(
+                k,
+                {
+                    '$inc': {
+                        'c': self._counters.pop(k),
+                    },
+                },
+                upsert=True,
+                attribute="n"
+            )
 
 
 def main():
--- typhoon/server/handlers/uvb.py
+++ typhoon/server/handlers/uvb.py
@@ -1,23 +1,10 @@
-import logging
 from tornado.gen import coroutine
 
 from typhoon.server.handlers import base
-from typhoon.server.settings import settings
-from typhoon.server.clients.mongo_client import BaseMongoClient
 
 
 class CountingHandler(base.BaseHandler):
-    def initialize(self):
-        assert settings.get('db', None) is not None
-        self.client = BaseMongoClient('test', settings)
-
-    @coroutine
-    def on_finish(self):
-        if self.name is not None:
-            self.client.update(self.get_argument('name'), {'$inc': {'c': 1}}, upsert=True, attribute="n")
-
-
     @coroutine
     def get(self):
-        self.name = self.get_argument("name", None, True)
+        self.application._counters[self.get_argument('name')] += 1
         self.write("yolo")
